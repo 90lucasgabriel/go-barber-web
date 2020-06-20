27bfe8fad6d0d75e95bb40a80b2467ef71e269cd
Lucas Teixeira <lucas.teixeira@webjump.com.br>
2020-06-20 17:57:13 -0300

    40 - Update: Test, assert and mock useHistory;

--- src/__tests__/pages/Signin.spec.tsx
+++ src/__tests__/pages/Signin.spec.tsx
@@ -1,18 +1,34 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
+
 import Signin from '../../pages/Sigin';
+
+const mockedHistoryPush = jest.fn();
 
 jest.mock('react-router-dom', () => {
   return {
-    useHistory: jest.fn(),
+    useHistory: () => ({
+      push: mockedHistoryPush,
+    }),
     Link: ({ children }: { children: React.ReactNode }) => children,
   };
 });
 
 describe('Signin Page', () => {
   it('should be able to signin ', () => {
-    const { debug } = render(<Signin />);
+    // Arrange
+    const { getByPlaceholderText, getByText } = render(<Signin />);
 
-    debug();
+    const emailField = getByPlaceholderText('E-mail');
+    const passwordField = getByPlaceholderText('Senha');
+    const buttonElement = getByText('Entrar');
+
+    // Act
+    fireEvent.change(emailField, { target: { value: 'johndoe@example.com' } });
+    fireEvent.change(passwordField, { target: { value: '123456' } });
+    fireEvent.click(buttonElement);
+
+    // Assert
+    expect(mockedHistoryPush).toHaveBeenCalledWith('/dashboard');
   });
 });
